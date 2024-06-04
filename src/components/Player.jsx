38fa97aae09d1348a5221cda0433b91c03db2cbd
airlorm <elormnizi@gmail.com>
2024-06-04 10:06:31 +0300

    add functionality for userboard

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChangePlayerName(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        value={playerName}
+        required
+        onChange={handleChangePlayerName}
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span>
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
